35dbd469531df0e3775a0f5e95f970fd46d7fbd6
KatyKateB <79481011+KatyKateB@users.noreply.github.com>
2021-04-08 10:30:54 -0600

    updates js with date rules

--- index.js
+++ index.js
@@ -31,7 +31,13 @@ let month = months[now.getMonth()];
 let day = days[now.getDay()];
 let date = now.getDate();
 let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
 let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 let year = now.getFullYear();
 
 currentDate.innerHTML = `${day}  ${hours}:${minutes}`;
